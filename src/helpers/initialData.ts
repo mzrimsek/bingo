16fc8a58fe98a1fbee9e5ace95df99a448f11345
Mike Zrimsek <mikezrimsek@gmail.com>
2021-08-24 00:19:41 -0400

    fix persisting the data happening every time state changes - should only execute on initial page load

--- src/helpers/initialData.ts
+++ src/helpers/initialData.ts
@@ -1,6 +1,8 @@
 import { BingoBoardOption, BingoSquareData } from 'models';
 import { getBoardUrlFromName, retrieveBingoBoard, retrieveSelectedBingoBoardOption } from 'helpers';
 
+import { persistSelectedBingoBoardName } from './localstorage';
+import { useEffect } from 'react';
 import { useSearchParam } from 'react-use';
 
 export const getInitialSelectedBingoBoardOption: () => BingoBoardOption = () => {
@@ -8,6 +10,7 @@ export const getInitialSelectedBingoBoardOption: () => BingoBoardOption = () => 
   if (boardQueryParam) {
     const url = getBoardUrlFromName(boardQueryParam);
     if (url) {
+      useEffect(() => persistSelectedBingoBoardName(boardQueryParam), []);
       return {
         label: boardQueryParam,
         url
