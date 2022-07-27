19b7688722a5b0e8e65eefd2261c91660e4d1205
Mike Zrimsek <mikezrimsek@gmail.com>
2022-07-27 19:51:08 -0400

    reimplemented the extremely useful useWidth hook that was stupidly removed also as a minor update to the project.

---
+++ src/hooks/useWidth.ts
@@ -0,0 +1,19 @@
+import { Breakpoint, Theme, useMediaQuery, useTheme } from "@mui/material";
+
+type BreakpointOrNull = Breakpoint | null;
+
+/**
+ * Be careful using this hook. It only works because the number of
+ * breakpoints in theme is static. It will break once you change the number of
+ * breakpoints. See https://reactjs.org/docs/hooks-rules.html#only-call-hooks-at-the-top-level
+ */
+export function useWidth() {
+  const theme: Theme = useTheme();
+  const keys: readonly Breakpoint[] = [...theme.breakpoints.keys].reverse();
+  return (
+    keys.reduce((output: BreakpointOrNull, key: Breakpoint) => {
+      const matches = useMediaQuery(theme.breakpoints.up(key));
+      return !output && matches ? key : output;
+    }, null) || 'xs'
+  );
+}
